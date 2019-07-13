5298061e605b3885b357812bd800e8b0aac221e4
bruce2tr <bruce.trammell@gmail.com>
2019-07-12 20:56:48 -0400

    table displaying and prompt returning user selection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,10 +21,34 @@ function loadTable() {
   var mySQLquery = "SELECT * FROM products";
   connection.query(mySQLquery, function(err, res) {
     if (err) throw err;
-    console.log(res);
+    // console.log(res);
+    
     // for (var i = 0; i < res.length; i++) {
-    //   console.log();
+    //   console.log(res[i].item_id + "  " + res[i].product_name + " || Department: " + res[i].department_name + " || Price: " + res[i].price + " || Qty: " + res[i].stock_quantity);
     // }
-    connection.end();
+
+    console.table(res, ["item_id", "product_name", "department_name", "price", "stock_quantity"]);
+
+    customerBuy();
   });
 }
+
+function customerBuy(){
+inquirer.prompt([{
+    name: 'Product_Choice',
+    type: 'number',
+    message: 'Enter the item_id of the product you would like to buy'
+},
+{
+    name: "Quantity",
+    type: "number",
+    message: "How many would you like to buy?"
+}])
+.then(function(answer){
+connection.query("SELECT * FROM products WHERE ?", {item_id: answer.Product_Choice}, function(err, res){
+    if (err) throw err;
+    console.log(res);
+connection.end();
+})
+})
+};
